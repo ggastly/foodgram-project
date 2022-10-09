68bb0fe142f582ef9823768187f9cd47f3a6a7f5
Денис <zolkovd@yandex.ru>
2022-10-09 07:01:37 +0400

    pep8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,5 @@
-from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
+from django.urls import include, path
 from api.views import (IngredientViewSet, RecipeViewSet, TagViewSet,
                        UsersViewSet)
 
